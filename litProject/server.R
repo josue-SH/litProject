5daaca7c1f20aad7b0a6f1323d5dc48cb143be90
josue-SH <jsanchezhernandez21@amherst.edu>
2024-01-31 18:34:03 -0800

    Edited download function from txt to zip file

--- litProject/server.R
+++ litProject/server.R
@@ -1,20 +1,27 @@
 library(shiny)
+library(shinyjs)
 library(tesseract)
 library(magick)
+library(zip)
 
 # Define server
 server <- function(input, output) {
-  converted_text <- reactiveVal(NULL)
+  #converted_text <- reactiveVal(NULL)
   
   observeEvent(input$convert_btn, {
     req(input$pdf_file)
     pdf_file_path <- input$pdf_file$datapath
     
+    # Create a temporary directory
+    temp_dir <- tempdir()
+    dir.create(temp_dir, showWarnings = FALSE)
+    setwd(temp_dir)
+    
     # Convert PDF to PNG
     png_files <- pdftools::pdf_convert(pdf_file_path, dpi = 600)
     
     # Process PNG files using Magick package
-    input_images <- lapply(png_files, magick::image_read)
+    magick_images <- lapply(png_files, magick::image_read)
     
     # Setting up the Tesseract OCR engine
     eng <- tesseract::tesseract("eng")
@@ -33,26 +40,35 @@ server <- function(input, output) {
     }
     
     # Loop through image directory
-    for (j in seq_along(input_images)) {
+    for (j in seq_along(magick_images)) {
       file_conn <- file(paste0("./texts/text_", j, ".txt"))
-      writeLines(generate_txt(input_images[[j]]), file_conn)
+      writeLines(generate_txt(magick_images[[j]]), file_conn)
       close(file_conn)
     }
     
-    converted_text(readLines(paste0("./texts/text_", 1:length(input_images), ".txt")))
+    zip_file <- zip::zip(zipfile = "textFiles.zip", 
+                        list.files(temp_dir, full.names = TRUE))
+    
+    #converted_text(readLines(paste0("./texts/text_", 1:length(magick_images), ".txt")))
     
     #file.remove(png_files)
     #file.remove(list.files("./texts"))
   
+    output$download_txt <- downloadHandler(
+      filename = function() {
+        paste("converted_text", ".zip", sep = "")
+      },
+      content = function(file) {
+        file.copy(zip_file, file)
+      }
+    )
+    
+    # Enable download button
+    shinyjs::enable("download_zip")
+    
     })
   
-  output$download_txt <- downloadHandler(
-    filename = function() {
-      paste("converted_text", ".txt", sep = "")
-    },
-    content = function(file) {
-      writeLines(converted_text(), file)
-    }
-  )
+  # Disable the download button intially
+  shinyjs::disable("download_zip")
   
 }
--- litProject/ui.R
+++ litProject/ui.R
@@ -1,11 +1,14 @@
 library(shiny)
+library(shinyjs)
 library(tesseract)
 library(magick)
+library(zip)
 
 # Define UI
 ui <- fluidPage(
   titlePanel("PDF to TXT Converter"),
   fileInput("pdf_file", "Choose a PDF file"),
   actionButton("convert_btn", "Convert to TXT"),
+  useShinyjs(),
   downloadButton("download_txt", "Download TXT")
 )
